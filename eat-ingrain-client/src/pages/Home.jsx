c6e0d1d4f409fc264cbf022e33431615547103a9
marklim61 <mark.lim288580@gmail.com>
2024-09-23 17:40:56 -0700

    fixed grainy in the home page

--- eat-ingrain-client/src/pages/Home.jsx
+++ eat-ingrain-client/src/pages/Home.jsx
@@ -24,25 +24,24 @@ const Home = () => {
     <>
     <Navbar/>
     {/* Center grainy */}
-    <div className="mx-auto w-4/5 min-w-[300px] max-w-[500px] h-full pt-16"> 
+    <div className="flex justify-center items-center w-full h-screen"> 
       <img 
         src={grainy} 
-        alt="Character" 
-        width="100%" 
-        className="pointer-events-none" />
+        alt="Character"
+        className="pointer-events-none w-4/5 max-w-[500px] min-w-[300px]" />
     </div>
 
     {/* Speech Bubbles */}
     <div className="absolute top-0 z-20 w-screen h-screen flex justify-center items-center">
       {isMobile ? (
-        <div className="absolute bottom-0 mb-10 flex space-x-4">
+        <div className="absolute bottom-20 mb-20 mt-20 flex space-x-4">
           <SpeechBubbleMobile speech="About" link={"/about"} />
           <SpeechBubbleMobile speech ="Events" link={"/events"} />
           <SpeechBubbleMobile speech="Shop" link={"/shop"} />
           <SpeechBubbleMobile speech="Contact" link={"/contact"} />
         </div>
       ) : (
-      <div className="fixed z-20 mx-auto grid grid-cols-2 grid-rows-2 w-4/5 h-full pt-16 gap-6">
+      <div className="fixed z-20 mx-auto grid grid-cols-2 grid-rows-2 w-4/5 h-full pt-16 pb-16 gap-6">
         <SpeechBubble speech="About" position={"top-left-tail"} link={"/about"} />
         <SpeechBubble speech="Events" position={"top-right-tail"} link={"/events"} />
         <SpeechBubble speech="Shop" position={"bottom-left-tail"} link={"/shop"}/>
